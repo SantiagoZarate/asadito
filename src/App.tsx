adda07b82abf8de51f58e8303ff64207a1219937
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-12 15:05:17 -0300

    chore: repot initialized with tailwindcss v4

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h2>Hola</h2>
+      <p>Hola</p>
+      <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+      </ul>
+    </div>
+  );
+}
